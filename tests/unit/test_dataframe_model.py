9badf447769f36dcf4280c5ba30b2541eac70d20
Keegan Walters <walky035@mymail.unisa.edu.au>
2023-09-23 15:51:42 +0930

    Initial commit - test_dataframe

---
+++ tests/unit/test_dataframe_model.py
@@ -0,0 +1,16 @@
+import pytest
+import pandas as pd
+from models.dataframe import DataFrameModel
+
+
+
+@pytest.fixture
+def empty_dataframe_model():
+    return DataFrameModel()
+
+def test_new_dataframe(empty_dataframe_model):
+    empty_dataframe_model.new_dataframe('file_path', 'dataset_name')
+    assert len(empty_dataframe_model._snapshots) == 1
+
+if __name__ == '__main__':
+    pytest.main()
