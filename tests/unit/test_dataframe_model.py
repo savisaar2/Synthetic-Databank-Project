2a623ec5d05f949cc10369fde93c08205d22c5f5
Keegan Walters <walky035@mymail.unisa.edu.au>
2023-09-24 11:57:45 +0930

    test - dataframe_model testing

--- tests/unit/test_dataframe_model.py
+++ tests/unit/test_dataframe_model.py
@@ -1,14 +1,20 @@
 import pytest
 import pandas as pd
 import sys
-sys.path.append('../../')
-from models.dataframe import DataFrameModel
+import os
+
+for path in sys.path:
+    print(path)
+
+from ...models.dataframe import DataFrameModel
 
 @pytest.fixture
 def empty_dataframe_model():
     return DataFrameModel()
 
-# Test cases for DataFrameModel
 def test_new_dataframe(empty_dataframe_model):
     empty_dataframe_model.new_dataframe('file_path', 'dataset_name')
     assert len(empty_dataframe_model._snapshots) == 1
+
+if __name__ == '__main__':
+    pytest.main()
