ab1b54a806a22af3b9ae7432cde74ba025852d7b
Htay Htat <htay.htat@gmail.com>
2023-09-25 11:33:10 +1000

    Add widgets for plotting in Analyse view.

--- views/pages/analyse.py
+++ views/pages/analyse.py
@@ -1,4 +1,4 @@
-from customtkinter import CTkFrame
+from customtkinter import CTkFrame, CTkLabel, CTkButton, CTkOptionMenu, CTkEntry, CTkScrollbar
 from .base import BaseView
 
 class AnalyseView(BaseView):
@@ -31,6 +31,34 @@ class AnalyseView(BaseView):
 
         self.table_frame = CTkFrame(self, fg_color="gray20")
         self.table_frame.pack(fill="both", padx=20, pady=(0, 20), expand=True)
+
+        # Visualisation options 
+        self.v_frame = CTkFrame(self.parent_frame, fg_color="transparent")
+        self.v_frame.pack(fill='x', pady=(20, 0), padx=20)
+        self.graph_label = CTkLabel(self.v_frame, text="Graph Style:", anchor="w")
+        self.graph_label.pack(side="left", padx=(8, 0))
+        self.graphing_options = ["------", "Box", "Histogram", "Line", "Scatter", "Violin"]
+        self.graph_option_menu = CTkOptionMenu(
+            self.v_frame, fg_color="gray10", width=3, values=self.graphing_options, 
+            command=lambda option: self.reconfig_widgets(option, "graph")
+            )
+        self.graph_option_menu.pack(side="left", padx=(8, 0))
+        self.variable_a_label = CTkLabel(self.v_frame, text="Variable:", anchor="w")
+        self.variable_a_label.pack(side="left", padx=(8, 0))
+        self.variable_a_option_menu = CTkOptionMenu(
+            self.v_frame, fg_color="gray10", width=3, state="disabled", values=("------",), 
+            command=lambda option: self.reconfig_widgets(option, "var_a")
+            )
+        self.variable_a_option_menu.pack(side="left", padx=(8, 0))
+        self.variable_b_label = CTkLabel(self.v_frame, text="Variable:", anchor="w")
+        self.variable_b_option_menu = CTkOptionMenu(
+            self.v_frame, fg_color="gray10", width=3, state="disabled", values=("------",), 
+            command=lambda option: self.reconfig_widgets(option, "var_b")
+            )
+        self.plot_button = CTkButton(
+            self.v_frame, text="Plot", corner_radius=5, border_spacing=5, anchor="center", state="disabled"
+            )
+        self.plot_button.pack(side="right", padx=(8, 8))
 
     def reconfig_widgets(self, option, option_set): 
         """Tottle (disable or enable) the appropriate button based on whether a valid option is selected.
