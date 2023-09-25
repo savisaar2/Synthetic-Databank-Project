927695489f5eaab8d7d43c12c5d9c490cd94d22e
Alex Kulbach <savisaar2@gmail.com>
2023-09-25 17:37:12 +0930

    Implemented search bar row widgets.

--- views/pages/library.py
+++ views/pages/library.py
@@ -1,4 +1,4 @@
-from customtkinter import CTkFrame
+from customtkinter import CTkFrame, CTkFont, CTkLabel, CTkEntry, CTkButton
 from .base import BaseView
 
 class LibraryView(BaseView):
@@ -22,12 +22,33 @@ class LibraryView(BaseView):
         """Renders widgets on the LibraryView page."""
         content_frame = self._create_frame(parent_frame=self, padx=20, pady=(0, 20))
         
-        row_1 = self._create_frame(parent_frame=content_frame)
+        row_1 = self._create_frame(parent_frame=content_frame, pady=20)
         row_2 = self._create_frame(parent_frame=content_frame)
         row_3 = self._create_frame(parent_frame=content_frame)
         row_4 = self._create_frame(parent_frame=content_frame)
+
+        self.search_label = self._create_label(row_1, str.upper("search"), "left")
+        self.search_input = self._create_entry(row_1, "left")
+
+        self.import_button = self._create_button(row_1, "Import", "left")
+        self.new_button = self._create_button(row_1, "New", "left")
 
     def _create_frame(self, parent_frame, padx=0, pady=0):
         frame = CTkFrame(parent_frame)
         frame.pack(fill="x", padx=padx, pady=pady)
         return frame
+    
+    def _create_label(self, frame, text="Text Here", side=None):
+        label = CTkLabel(frame, text=text, font=CTkFont(size=18, weight="normal"))
+        label.pack(padx=20, anchor="w", side=side)
+        return label
+    
+    def _create_entry(self, frame, side):
+        entry = CTkEntry(frame, height=45)
+        entry.pack(side=side, expand=True, fill="x", padx=10)
+        return entry
+    
+    def _create_button(self, frame, text="Text Here", side="top"):
+        button = CTkButton(frame, corner_radius=5, height=40, image="", border_spacing=10, text=text, anchor="n")
+        button.pack(side=side, fill="x", padx=5)
+        return button
