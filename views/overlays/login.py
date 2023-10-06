9980a7b50dc0454efc98bb89b5964c8aab070f8c
Alex Kulbach <savisaar2@gmail.com>
2023-10-06 18:04:23 +1030

    Apply frontend for authentication view.

--- views/overlays/login.py
+++ views/overlays/login.py
@@ -1,4 +1,5 @@
-from customtkinter import CTkFrame, CTkLabel
+from customtkinter import CTkFrame, CTkLabel, CTkImage, CTkEntry, CTkButton
+from PIL import Image
 
 class LoginView(CTkFrame):
     def __init__(self, root, *args, **kwargs):
@@ -24,6 +25,29 @@ class LoginView(CTkFrame):
             relwidth=1, relheight=1
         )
         self.overlay_frame.lower()
+
+        self.login_frame = CTkFrame(self.overlay_frame)
+        self.login_frame.place(rely=0.5, relx=0.5, anchor="c")
+
+        self.logo_image = CTkImage(Image.open("views/static/images/unisa_logo.png"), size=(150, 150))
+
+        self.logo_label = CTkLabel(self.login_frame, text="", image=self.logo_image)
+        self.logo_label.pack(padx=100,pady=10)
+
+        # Add the username_entry and center it horizontally
+        self.username_entry = CTkEntry(self.login_frame, corner_radius=5, placeholder_text="username", justify="center", font=("Arial", 16), width=250, height=40, border_width=1)
+        self.username_entry.pack(padx=20, pady=5)
+
+        # Add the password_entry and center it horizontally
+        self.password_entry = CTkEntry(self.login_frame, corner_radius=5, placeholder_text="password", justify="center", show="*", font=("Arial", 16), width=250, height=40, border_width=1)
+        self.password_entry.pack(pady=5)
+
+        # Add the login_button and center it horizontally
+        self.login_button = CTkButton(self.login_frame, corner_radius=5, text="Login", font=("Arial", 16), width=250, height=40)
+        self.login_button.pack(pady=10)
+
+        self.msg_label = CTkLabel(self.login_frame, text="")
+        self.msg_label.pack(pady=(0, 50))
 
         self.logo_label = CTkLabel(self.overlay_frame, text="test")
         self.logo_label.pack(padx=100,pady=10)
