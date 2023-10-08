ea3d42ada7440442bbf3f633ec92058924f4d8f9
Keegan Walters <walky035@mymail.unisa.edu.au>
2023-10-08 22:14:00 +1030

    feat: add Hanh's manip algorithms

--- models/manipulations.py
+++ models/manipulations.py
@@ -2,6 +2,10 @@ import pandas as pd
 import random
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.preprocessing import StandardScaler
 
 class ManipulationsModel():
     def __init__(self):
@@ -50,26 +54,20 @@ class ManipulationsModel():
 
         match sub_action:
             case "Add Random Custom Value":
-                # Function to add a random custom value to the selected column                
-                column_data_type = df[column].dtype # Get the data type of the column                
-                # Check for datatype mismatch
-                try:
-                    if column_data_type == 'int64':
-                        a = int(float(a))  
-                    elif column_data_type == 'float64':
-                        a = float(a)
-                    # Get the number of rows in the DataFrame
-                    num_rows = df.shape[0]
-                    # Generate a random set of row indices to replace       
-                    random_row_indices = random.sample(range(num_rows), int(b))                    
-                    # Replace the selected rows with the custom value
-                    df.loc[random_row_indices, column] = a                                   
-                    return df
-                except ValueError:                                 
-                    print(f"Error: Datatype mismatch. Custom value '{a}' is incompatible with '{column_data_type}'.")
+                # Function to add random custom value to the selected column  
+                # a = custom value; b = number of random custom values; column = the selected column
+
+                # Get the number of rows in the DataFrame
+                num_rows = df.shape[0]
+                # Generate a random set of row indices to replace       
+                random_row_indices = random.sample(range(num_rows), b)                    
+                # Replace the selected rows with the custom value
+                df.loc[random_row_indices, column] = a                                   
+                return df
 
             case "Add Missing":
-                a=int(a) # Convert a to int for numbers of value to replace
+                # a = number of missing values; column = the selected column
+
                 match column:                 
                     case "" | None:
                         # Function to add missing value to the entrie dataset                     
@@ -89,29 +87,48 @@ class ManipulationsModel():
                         return df
                     
             case "Add Outliers Z-score":
-                # Functionto add outliers to the selected to column
-                # Check if the selected column is numeric
-                if pd.api.types.is_numeric_dtype(df[column]):
-                    # Add outliers to the selected variable
-                    z_scores = np.abs((df[column] - df[column].mean()) / df[column].std())
-                    # Identify the rows where z-scores exceed the threshold
-                    outlier_indices = np.where(z_scores > float(a))[0]
-                    # Ensure the target column has a floating-point dtype
-                    df[column] = df[column].astype(float)
-                    # Introduce outliers using .loc to set values
-                    outlier_values = np.random.uniform(5, 10, len(outlier_indices)).astype(float) #### should user has option to enter the range
-                    df.loc[df.index[outlier_indices], column] += outlier_values
-                    return df
-                else:
-                     print(f"'{column}' is not a valid numeric column in the dataset.") 
+                # Functionto add outliers to the selected to column  based on z-score              
+                # a = z-score threshold; column = the selected column
+                               
+                z_scores = np.abs((df[column] - df[column].mean()) / df[column].std())
+                # Identify the rows where z-scores exceed the threshold
+                outlier_indices = np.where(z_scores > float(a))[0]
+                # Ensure the target column has a floating-point dtype
+                df[column] = df[column].astype(float)
+                # Introduce outliers using .loc to set values
+                outlier_values = np.random.uniform(5, 10, len(outlier_indices)).astype(float) #### should user has option to enter the range?
+                df.loc[df.index[outlier_indices], column] += outlier_values
+                return df
 
             case "Add Outliers Percentile":
-                # Fucntion to add outliers directly to the selected variable based on percentiles
-                lower_bound = np.percentile(df[column], int(a))
-                upper_bound = np.percentile(df[column], int(b))
-                outliers = np.random.uniform(lower_bound, upper_bound, len(df))
-                df[column] += outliers
-                return df 
+                # Functionto add outliers to the selected to column  based on lower bound and upper bound
+                # a= number of outliers; column = the selected column
+
+                # Calculate the IQR (Interquartile Range) for the selected column
+
+                Q1 = df[column].quantile(0.25)
+                Q3 = df[column].quantile(0.75)
+                IQR = Q3 - Q1
+
+                # Calculate lower_bound and upper_bound for outliers
+                lower_bound = Q1 - 1.5 * IQR
+                upper_bound = Q3 + 1.5 * IQR
+
+                # Randomly select n rows from the DataFrame
+                selected_rows = df.sample(n=a, replace=True)
+
+                # Generate n random outliers below the lower bound and above the upper bound
+                lower_outliers = np.random.uniform(lower_bound - 1.5 * IQR, lower_bound, a // 2)
+                upper_outliers = np.random.uniform(upper_bound, upper_bound + 1.5 * IQR, (a- (a // 2)))
+                outliers = np.concatenate([lower_outliers, upper_outliers])
+
+                # Add the outliers to the selected rows in the BMI column
+                selected_rows[column] = outliers[:a]
+
+                # Update the modified rows in the original DataFrame
+                df.update(selected_rows)
+
+                return df  
             
             case "Add Outliers Min/Max":
                 # Insert function here!!!
@@ -134,11 +151,15 @@ class ManipulationsModel():
         match sub_action:
             case "Duplicate":
                 # Function to add an extra column by duplicating an existing column
+                # a = new column's name; column = the selected column
+
                 df_with_duplicate_column = df.copy()
                 df_with_duplicate_column[a] = df_with_duplicate_column[column]
                 return df_with_duplicate_column
+            
             case "New":
                 # Function to add new column to dataframe.
+                # a = new column's name
                 df[a]=None
                 return df
             case "Feature Engineering Polynominal Features":
@@ -153,48 +174,92 @@ class ManipulationsModel():
 
         match sub_action:
             case "Algorithmic PCA":
-                # Function to reduce dataset dimensionality using PCA
-                # All features must be numerical
-                # Numbers of column/features to keep must be less than the total features
+                # Function to reduce dataset dimensionality using PCA     
+                # a = target (dependent) column); b = number of features to keep
+
+                # Extract the features (X) from the DataFrame
+                X = df.drop(columns=[a])  
+                #Initialize and fit the PCA model
+                pca = PCA(n_components=b)
+                X_pca = pca.fit_transform(X)
+                # Create a DataFrame with the reduced dimensionality
+                columns = [f'PC{i+1}' for i in range(b)]
+                df_pca = pd.DataFrame(data=X_pca, columns=columns)
+                # Add the target column back to df_pca 
+                df_pca[a] = df[a]     
+                print(df_pca.head(10))
+                return df_pca
+            
+            case "Algorithmic LDA":                           
+                # Function to reduce dataset dimensionality using LDA  
+                # a = target (dependent) column); b = number of features to keep
+
+                # Extract the features (X) and target variable (y) from the DataFrame
+                X = df.drop(columns=[a])
+                y = df[a]
+
+                # Initialize and fit the LDA model
+                lda = LinearDiscriminantAnalysis(n_components=b)
+                X_lda = lda.fit_transform(X, y)
+
+                # Create a DataFrame with the reduced dimensionality
+                columns = [f'LD{i + 1}' for i in range(b)]
+                df_lda = pd.DataFrame(data=X_lda, columns=columns)
                 
-                print(a,column)
-                
-                # Check if there are non-numeric features
-                non_numeric_columns = df.select_dtypes(exclude=['int64','int32','float64','float32']).columns
-                if not non_numeric_columns.empty:
-                    print("Cannot perform PCA on non-numeric features:")
-                    print(non_numeric_columns)
-                else:
-                    # Get the total number of variables (features)
-                    total_variables = df.shape[1] - 1  # Subtract 1 for the target column
-                    num_components = int(b)
-                    if num_components >= df.shape[1]:
-                        print("Error: num_components should be less than the total number of features - 1")
-                    else:
-                        # Extract the features (X) from the DataFrame
-                        X = df.drop(columns=[a])  
-                        #Initialize and fit the PCA model
-                        pca = PCA(n_components=num_components)
-                        X_pca = pca.fit_transform(X)
-                        # Create a DataFrame with the reduced dimensionality
-                        columns = [f'PC{i+1}' for i in range(num_components)]
-                        df_pca = pd.DataFrame(data=X_pca, columns=columns)
-                        # Add the target column back to df_pca 
-                        df_pca[a] = df[a]     
-                        print(df_pca.head(10))
-                        return df_pca
-            case "Algorithmic LDA":
-                # Insert function here!!!
-                pass
+                df_lda[a] = y # Add the target column back to df_lda if needed
+                return df_lda
+            
             case "Algorithmic SVD":
-                # Insert function here!!!
-                pass
+                # Function to reduce dataset dimensionality using SVD
+                # a: selected column; b: numbers of column to keep                
+                # b > 1 and b <= df.shape[1]-1:           
+
+                # Extract the features (X) from the DataFrame
+                X = df.drop(columns=[a])
+
+                # Initialize and fit the TruncatedSVD model
+                svd = TruncatedSVD(n_components=b)
+                X_svd = svd.fit_transform(X)
+
+                # Create a DataFrame with the reduced dimensionality
+                columns = [f'SVD{i+1}' for i in range(b)]
+                df_svd = pd.DataFrame(data=X_svd, columns=columns)
+
+                # Add the target column back to df_svd if needed
+                df_svd[a] = df[a]                       
+                return df_svd
+            
             case "Algorithmic Sklearn":
-                # Insert function here!!!
-                pass
+                # Function to reduce dataset dimensionality using SKlearn Feature Selection
+                # a = target (dependent) column; b = number of columns(features) to retain
+
+                X = df.drop(column, axis=1)
+                y = df[column]
+
+                # Standardize the features (important for feature selection)
+                scaler = StandardScaler()
+                X_scaled = scaler.fit_transform(X)                    
+
+                # Create a SelectKBest model using ANOVA F-statistic (can change this metric)
+                select_best = SelectKBest(score_func=f_classif, k=b)
+
+                # Fit the SelectKBest model to your standardised data
+                X_selected = select_best.fit_transform(X_scaled, y)
+
+                # Get the indices of the selected features
+                selected_feature_indices = select_best.get_support(indices=True)
+
+                # Create a DataFrame with the reduced-dimensionality data
+                reduced_data = pd.DataFrame(data=X_selected, columns=[X.columns[i] for i in selected_feature_indices])
+
+                # Add the target variable back to the generated dataset
+                reduced_data[column] = y
+                return reduced_data
+
             case "Manual":
-                # Insert function here!!!
-                pass
+                # Funtion to remove the selected column
+                df = df.drop(column)
+                return df
 
     def remove_rows(self, sub_action, df, column, args, sme):   
         a, b, c = args["a"], args["b"], args["c"]  #unpack args
@@ -247,8 +312,10 @@ class ManipulationsModel():
     def change_column_name(self, sub_action, df, column, args, sme):   
         a, b, c = args["a"], args["b"], args["c"]  #unpack args
 
-        # Insert function here!!!
-        pass
+        # Function to rename selected column
+        df.rename(columns={column: a}, inplace=True)
+
+        return df
 
     def add_rows(self, sub_action, df, column, args, sme):   
         a, b, c = args["a"], args["b"], args["c"]  #unpack args
