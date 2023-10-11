65f904dac4bb7329ad5bc6c5afacff32c9fa0750
Alex Kulbach <savisaar2@gmail.com>
2023-10-11 20:19:48 +1030

    patched login return key bug.

--- controllers/login.py
+++ controllers/login.py
@@ -55,4 +55,5 @@ class LoginController:
         for user interactions with widgets on the view related to the login page.
         """
         self.frame.login_button.bind("<Button-1>", lambda event: self._authenticate_user(event))
-        self.frame.root.bind("<Return>", lambda event: self._authenticate_user(event))
+        self.frame.username_entry.bind("<Return>", lambda event: self._authenticate_user(event))
+        self.frame.password_entry.bind("<Return>", lambda event: self._authenticate_user(event))
